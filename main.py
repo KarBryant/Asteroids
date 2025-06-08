18c6fab9210af2418a80476582bf99f1a8d9ef07
Karson Bryant <karson.r.bryant@outlook.com>
2025-06-08 14:40:51 -0500

    created an in game clock and delta-time/fps count. set the game's FPS to 60.

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     print(f"Game Initialized: {pygame.get_init()}")
 
+## Set Variables For Game
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+## Main Game Loop
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = 1000/clock.tick(60)
 
 if __name__ == "__main__":
     main()
